a2d0da7352f36a5dbb6e818a440e5d0eae1b370d
math-shpark <psuhoo122065@gmail.com>
2022-03-20 13:49:21 +0900

    Updated on Mar 20 2022 13:49

---
+++ Chapter 6/Address.java
@@ -0,0 +1,27 @@
+package chapter06;
+
+public class Address {
+	private String city;
+	private String houseCode;
+	
+	public Address() {
+		
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getHouseCode() {
+		return houseCode;
+	}
+
+	public void setHouseCode(String houseCode) {
+		this.houseCode = houseCode;
+	}
+	
+}
